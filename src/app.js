86b1402428d13f4bd1e505ba57635e3ea9269ca6
sphinx-sphinx <iansoul7@gmail.com>
2020-06-07 04:03:59 +0300

    Init commit

---
+++ src/app.js
@@ -0,0 +1,85 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+
+// console.log(__dirname)
+// console.log(__filename)
+
+const app = express()
+
+//Define paths for express config
+const publicDirectoryPath = path.join(__dirname,'../public')
+const viewsPath = path.join(__dirname,'../template/views')
+const partialsPath = path.join(__dirname,'../template/partials')
+
+//Setup handlebars engine and views location
+app.set('view engine','hbs')
+app.set('views',viewsPath)
+hbs.registerPartials(partialsPath)
+
+//Setup static directory to serve 
+app.use(express.static(publicDirectoryPath))
+
+
+app.get('', (req,res) => {
+    res.render('index',{
+        title: 'Weather App',
+        name: 'Hermes Margiela'
+
+    })
+})
+
+app.get('/about', (req,res) => {
+    res.render('about',{
+        title: 'About Me',
+        name: 'Hermes Margiela'
+    })
+})
+
+app.get('/help', (req,res) => {
+    res.render('help',{
+        title: 'Help Page',
+        name: 'Hermes Margiela'
+    })
+})
+
+app.get('/help/*', (req,res) => {
+    res.render('404',{
+        title: '404',
+        name: 'Hermes Margiela',
+        erroMessage: 'Help Article Not Found'
+    })
+})
+
+
+app.get('*', (req,res) =>{
+    res.render('404', {
+        title: '404',
+        name:'Hermes Margiela',
+        errorMessage:'Page Not FOund'
+    })
+})
+
+
+// app.get('',(req,res) =>{
+//     res.send('Hello Express!')
+// })
+
+// app.get('/help',(req,res) =>{
+//     res.send('Help Page')
+// })
+
+// app.get('/about',(req,res) =>{
+//     res.send('About Page')
+// })
+
+
+app.get('/weather',(req,res) =>{
+    res.send('Weather Page')
+})
+
+
+app.listen(3000,() =>{
+    console.log('Server is up and running on port 3000')
+})
+
